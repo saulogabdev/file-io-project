8c355377f14bf0c84b9748e11dedb354e688e1a9
Saulo Gabriel <saulo.gab.san.souz@gmail.com>
2025-11-05 08:52:09 -0300

    Refactor notification factory methods.

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
 // Classe base da fábrica
 abstract class FabricaNotificacao {
-    public abstract void enviar(String mensagem);
+    public abstract void criarNotificacao(String mensagem);
 }
 
 // Fábricas concretas
 class FabricaEmail extends FabricaNotificacao {
-    public void enviar(String mensagem) {
+    public void criarNotificacao(String mensagem) {
         System.out.println("Enviando e-mail: " + mensagem);
     }
 }
 
 class FabricaSMS extends FabricaNotificacao {
-    public void enviar(String mensagem) {
+    public void criarNotificacao(String mensagem) {
         System.out.println("Enviando SMS: " + mensagem);
     }
 }
@@ -19,7 +19,7 @@ class FabricaSMS extends FabricaNotificacao {
 // Programa principal
 public class Main {
     public static void main(String[] args) {
-        FabricaNotificacao fabrica = new FabricaEmail();
-        fabrica.enviar("Bem-vindo!");
+        FabricaNotificacao fabrica = new FabricaEmail(); // Pode trocar para FabricaSMS
+        fabrica.criarNotificacao("Bem-vindo!");
     }
 }
